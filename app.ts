3d8947e965e65411bc3442671d760c00d92ff406
윤주 엄 <yunzoo0915@gmail.com>
2023-05-29 14:58:36 +0900

    chore: DB 정보 env파일환경설정완료

--- app.ts
+++ app.ts
@@ -1,5 +1,8 @@
-import mysql, { MysqlError } from 'mysql';
 import express, { Request, Response, NextFunction } from 'express';
+import mysql, { MysqlError, ConnectionConfig } from 'mysql';
+import dotenv from 'dotenv';
+
+dotenv.config(); // dotenv 패키지를 사용하여 환경 변수 로드
 
 const app = express();
 
@@ -11,12 +14,14 @@ app.listen('3000', () => {
   console.log('server on!');
 });
 
-const connection = mysql.createConnection({
-  host: "34.135.60.118",
-  user: "root",
-  password: "elicefirefighter",
-  database: "seongsudongelice",
-});
+const connectionConfig: ConnectionConfig = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+};
+
+const connection = mysql.createConnection(connectionConfig);
 
 connection.connect((err: MysqlError | null) => {
   if (err) throw err;
