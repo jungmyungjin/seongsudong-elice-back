9fb848e2faa46063fab2ff2352cb9f9b0aa8c473
윤주 엄 <yunzoo0915@gmail.com>
2023-05-29 14:44:14 +0900

    chore: DB연결 완료

--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
+import mysql, { MysqlError } from 'mysql';
 import express, { Request, Response, NextFunction } from 'express';
 
 const app = express();
@@ -6,6 +7,18 @@ app.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.send('welcome!');
 });
 
-app.listen('5000', () => {
+app.listen('3000', () => {
   console.log('server on!');
+});
+
+const connection = mysql.createConnection({
+  host: "34.135.60.118",
+  user: "root",
+  password: "elicefirefighter",
+  database: "seongsudongelice",
+});
+
+connection.connect((err: MysqlError | null) => {
+  if (err) throw err;
+  console.log("Connected to Google MySQL database");
 });
