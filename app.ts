11d02f6562522f1a11c94eaf86428b647a63c255
윤주 엄 <yunzoo0915@gmail.com>
2023-05-29 21:49:25 +0900

    Feat: user 스키마, 모델생성

--- app.ts
+++ app.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response, NextFunction } from 'express';
 import mysql, { MysqlError, ConnectionConfig } from 'mysql';
 import dotenv from 'dotenv';
-
 dotenv.config(); // dotenv 패키지를 사용하여 환경 변수 로드
+
+import { createMembersTable } from './src/models/members';
 
 const app = express();
 
@@ -14,16 +15,9 @@ app.listen('3000', () => {
   console.log('server on!');
 });
 
-const connectionConfig: ConnectionConfig = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-};
 
-const connection = mysql.createConnection(connectionConfig);
 
-connection.connect((err: MysqlError | null) => {
-  if (err) throw err;
-  console.log("Connected to Google MySQL database");
-});
+createMembersTable(); // createMembersTable 함수 호출
+
+
+
