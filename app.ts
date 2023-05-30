0b069e16cf0fb2070dd939f0735fe5a50118cf4d
Rki0 <pky00823@gmail.com>
2023-05-30 23:08:57 +0900

    feat: 댓글 생성 API 구현

--- app.ts
+++ app.ts
@@ -3,14 +3,6 @@ dotenv.config();
 
 import express, { Request, Response, NextFunction } from 'express';
 
-import { createMembersTable } from './src/models/members';
-import { createConnectionStatusTable } from './src/models/connection_status';
-import { createChatRoomTable } from './src/models/chat_room';
-import { createChatsTable } from './src/models/chats';
-import { createPostsTable } from './src/models/posts';
-import { createCommentsTable } from './src/models/comments';
-import { createSeatsTable } from './src/models/seats';
-import { createReservationTable } from './src/models/reservations';
 import commentRouter from './src/routes/comment-routes';
 
 const app = express();
