0ac61c8fec859d8f25579afac3d826b4116f91b9
Rki0 <pky00823@gmail.com>
2023-06-01 15:51:34 +0900

    feat: 댓글 삭제 기능 구현
    관리자, 일반 유저가 댓글을 삭제할 때 사용할 API를 구현했습니다.
    또한, 댓글 추가 API에서 일부 수정이 있습니다.
    외래키 참조를 통해 댓글 데이터와 유저 데이터를 함께 반환합니다.

--- src/routes/comment-routes.ts
+++ src/routes/comment-routes.ts
@@ -2,11 +2,15 @@ import express from 'express';
 import {
   createComment,
   getComments,
+  deleteComment,
+  deleteCommentAdmin,
 } from '../controllers/comments-controllers';
 
 const commentRouter = express.Router();
 
 commentRouter.get('/', getComments);
 commentRouter.post('/:postId', createComment);
+commentRouter.delete('/:postId', deleteComment);
+commentRouter.delete('/:postId/admin', deleteCommentAdmin);
 
 export default commentRouter;
