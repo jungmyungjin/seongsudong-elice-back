4379413cc42a7d1228dbe4b970e0899c3b11bbb1
윤주 엄 <yunzoo0915@gmail.com>
2023-06-02 18:03:28 +0900

    Chore: sql 테이블 변경 및 클라우드 DB 세팅 변경/ reservation_id: int > varchar(uuid), seat 테이블 reservation 관련정보 삭제, 시간대별 available 컬럼 추가

--- src/db/reservation.sql
+++ src/db/reservation.sql
@@ -1,6 +1,6 @@
 CREATE TABLE
     reservations (
-        id INT PRIMARY KEY AUTO_INCREMENT,
+        resevation_id VARCHAR(50) PRIMARY KEY NOT NULL DEFAULT '',
         member_generation VARCHAR(50) NOT NULL,
         member_name VARCHAR(255) NOT NULL,
         member_email VARCHAR(255) NOT NULL,
@@ -9,6 +9,7 @@ CREATE TABLE
         end_time TIME NOT NULL,
         num_of_guests INT NOT NULL,
         seat_number VARCHAR(255) NOT NULL,
+        seat_type VARCHAR(255) NOT NULL,
         status ENUM('예약완료', '예약취소') NOT NULL,
         created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
         FOREIGN KEY (
@@ -17,5 +18,5 @@ CREATE TABLE
             member_email
         ) REFERENCES members (generation, name, email),
         FOREIGN KEY (seat_number) REFERENCES seats (seat_number),
-        INDEX (member_email)
+        INDEX (member_email) -- 빠른 예약조회를 위해 인덱스 정렬
     );
