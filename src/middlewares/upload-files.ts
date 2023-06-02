70332a0043714fcd2af647c4edcc347b966bf340
alloboo <hye870425@gmail.com>
2023-06-02 11:36:40 +0900

    multer 미들웨어 추가

---
+++ src/middlewares/upload-files.ts
@@ -0,0 +1,34 @@
+import multer, { StorageEngine, FileFilterCallback } from 'multer';
+import path from 'path';
+import { Request } from 'express';
+
+const storage: StorageEngine = multer.diskStorage({
+  destination: function (req: Request, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req: Request, file, cb) {
+    const ext = path.extname(file.originalname);
+    const filename = file.originalname.replace(ext, '');
+    cb(null, filename + '-' + Date.now() + ext);
+  },
+});
+
+const fileFilter = (
+  req: Request,
+  file: Express.Multer.File,
+  cb: FileFilterCallback,
+) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true); // 유효한 파일 형식인 경우 허용
+  } else {
+    cb(new Error('Invalid file format')); // 유효하지 않은 파일 형식인 경우 거부
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 2 * 1024 * 1024 },
+  fileFilter: fileFilter,
+});
+
+export default upload;
