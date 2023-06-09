5b7c5d0fd1edf3125612108df6d6f2e7c70ec9d6
[SW4/2반] 엄윤주 <yoonju.eom1@gmail.com>
2023-06-09 08:46:59 +0000

    Fix: 좌석확인 시간별로 true/false 잘못 나오는 이슈 해결(start_time/end_time time>string 타입변경으로 초단위 삭제)

--- src/controllers/reservations-controller.ts
+++ src/controllers/reservations-controller.ts
@@ -50,17 +50,17 @@ export const seatCheck = async (req: Request, res: Response): Promise<{ [seatNum
             const startTime = reservation.start_time;
             const endTime = reservation.end_time;
             const seatNumber = reservation.seat_number;
-            console.log('시작시간: ', startTime, '종료시간: ', endTime)
+            console.log('좌석', seatNumber, '시작시간: ', startTime, '종료시간: ', endTime)
 
-            if (startTime <= '10:00:00' && endTime >= '14:00:00') {
+            if (startTime <= '10:00' && endTime >= '14:00') {
                 seatAvailability[seatNumber].available10to14 = false;
             }
 
-            if (startTime <= '14:00:00' && endTime >= '18:00:00') {
+            if (startTime <= '14:00' && endTime >= '18:00') {
                 seatAvailability[seatNumber].available14to18 = false;
             }
 
-            if (startTime <= '18:00:00' && endTime >= '22:00:00') {
+            if (startTime <= '18:00' && endTime >= '22:00') {
                 seatAvailability[seatNumber].available18to22 = false;
             }
         });
