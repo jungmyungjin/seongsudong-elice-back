0106e038de41d9ef9c6def26fd8b2d0c6891c480
alloboo <hye870425@gmail.com>
2023-06-09 22:47:16 +0900

    Feat: 채팅방 생성 이벤트 작성

--- app.ts
+++ app.ts
@@ -1,16 +1,17 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import express, { Request, Response, NextFunction } from 'express';
-
 import commentRouter from './src/routes/comment-routes';
 import memberRouter from './src/routes/member-routes';
 //import authRouter from './src/routes/auth-routes'
 import adminRouter from './src/routes/admin-routes';
 import postRouter from './src/routes/post-routes';
 import reservationRouter from './src/routes/reservaton-routes';
+import chatRouter from './src/routes/chat-routes';
 
+import express from 'express';
 import session from 'express-session';
+import { RowDataPacket } from 'mysql2';
 import passport from 'passport';
 import cors from 'cors';
 import http from 'http';
@@ -21,6 +22,8 @@ import {
   googleCallbackRedirect,
   googleStrategy,
 } from './src/controllers/members-controllers';
+import { saveMessages, getAllMessages } from './src/utils/chat-utils';
+import con from './connection';
 
 const app = express();
 
@@ -61,16 +64,15 @@ app.use(
   }),
 );
 
-
 // Express 애플리케이션에 세션 미들웨어 추가
 app.use(session(sessionConfig));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', (req: Request, res: Response, next: NextFunction) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
 
 app.use('/uploads', express.static('uploads'));
 
@@ -79,8 +81,8 @@ app.listen(3000, () => {
 });
 
 // socket.io 서버
-server.listen(3001, () => {
-  console.log(`Socket server on 3001 !!`);
+server.listen(3002, () => {
+  console.log(`Socket server on!!`);
 });
 
 app.get('/auth/google', googleStrategy);
@@ -92,3 +94,41 @@ app.use('/api/comments', commentRouter);
 app.use('/api/posts', postRouter);
 app.use('/api/reservations', reservationRouter);
 //app.use('/', authRouter);
+app.use('/api/chat', chatRouter);
+
+
+// socket.io 연결
+io.on('connect', socket => {
+  console.log('connected!!!');
+
+  // 채팅방 생성 이벤트
+  socket.on('createChatRoom', async (email: string, messages: string) => {
+    // email에 해당하는 메세지 찾기
+    const checkChatRoomQuery = `SELECT * FROM chat_messages WHERE sender_email = ? LIMIT 1;`;
+    const checkResult = await con.promise().query(checkChatRoomQuery, [email]);
+    console.log(checkResult[0])
+
+    let roomId;
+  
+    if ((checkResult[0] as RowDataPacket).length > 0) {
+      // 첫 메세지가 아닐 경우, 결과에서 roomId 가져오기
+      roomId = (checkResult[0] as RowDataPacket).room_id;
+      console.log("It's first msg!");
+    } else {
+      // 첫 메세지일 경우, 채팅방 생성
+      const createChatRoomQuery = `INSERT INTO chat_rooms (member_email) VALUES (?);`;
+      const [result] = await con.promise().query(createChatRoomQuery, [email]);
+      const newRoomId = (result as RowDataPacket).insertId;
+      roomId = newRoomId;
+      console.log('ChatRoom created!') 
+    }
+
+    // 메세지 db 저장
+    await saveMessages(roomId, email, messages);
+    console.log('Messages saved!');
+
+    // 해당 채팅방의 모든 메세지 전달
+    socket.emit('chatMessages', getAllMessages(roomId));
+    console.log('Messages sent!');
+  });
+});
