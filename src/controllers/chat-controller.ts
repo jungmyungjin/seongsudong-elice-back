bc3ba74868366581532a90d470a919afe02382d1
alloboo <hye870425@gmail.com>
2023-06-09 19:38:11 +0900

    Feat: 채팅방리스트 조회 쿼리문 수정

--- src/controllers/chat-controller.ts
+++ src/controllers/chat-controller.ts
@@ -8,7 +8,17 @@ export const getChatRoomList = async (
   res: Response,
   next: NextFunction,
 ) => {
-  const getChatRoomListQuery = `SELECT * FROM chat_messages LEFT JOIN chat_rooms ON chat_messages.room_id = chat_rooms.room_id ORDER BY created_at LIMIT 1;`;
+  const getChatRoomListQuery = `SELECT chat_rooms.room_id, members.member_email, members.member_name, members.generation, chat_messages.message, chat_messages.sentAt
+  FROM chat_rooms
+  JOIN members ON chat_rooms.members_email = members.email
+  JOIN (
+      SELECT room_id, MAX(sentAt) AS latest_message_date
+      FROM chat_messages
+      GROUP BY room_id
+  ) AS latest_messages ON chat_rooms.room_id = latest_messages.room_id
+  JOIN chat_messages ON latest_messages.room_id = chat_messages.room_id AND latest_messages.latest_message_date = chat_messages.sentAt ORDER BY chat_messages.sendAt DESC;
+  `;
   const getChatRoomListResult = await con.promise().query(getChatRoomListQuery);
+
   return res.status(200).json(getChatRoomListResult);
 };
