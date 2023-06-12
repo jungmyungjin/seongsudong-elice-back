e7e0af3d0360aa43444cce32e4a52b72a0732e46
alloboo <hye870425@gmail.com>
2023-06-12 18:07:54 +0900

    Fix: socket 이벤트 수정

--- app.ts
+++ app.ts
@@ -8,6 +8,7 @@ import adminRouter from './src/routes/admin-routes';
 import postRouter from './src/routes/post-routes';
 import reservationRouter from './src/routes/reservaton-routes';
 import chatRouter from './src/routes/chat-routes';
+import accessRouter from './src/routes/access-routes';
 
 import express from 'express';
 import session from 'express-session';
@@ -18,16 +19,53 @@ import http from 'http';
 import { Server } from 'socket.io';
 import { OAuth2Client } from 'google-auth-library';
 
-import {
-  googleCallback
-} from './src/controllers/member2_controller';
-import { saveMessages, getAllMessages, getRoomId, getLatestMessage, getAllConnectionData } from './src/utils/chat-utils';
+// import {
+//   googleCallback,
+//   googleCallbackRedirect,
+//   googleStrategy,
+// } from './src/controllers/members-controllers';
+import { getRoomId, getAllMessages, getMembersMessages, createChatRoom, saveMessages, getLatestMessage, getAllConnectionData } from './src/utils/chat-utils';
 import con from './connection';
-import { googleLogin } from './src/controllers/member2_controller';
+// import { googleCallback, googleLogin } from './src/controllers/member2_controller';
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    // origin: true,
+    origin: 'http://localhost:3000',
+    credentials: true,
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+    exposedHeaders: ['set-cookie'],
+  }),
+);
+app.use(
+  session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: false,
+    // cookie: {
+    //   secure: false, // HTTPS가 아닌 경우 false로 설정
+    //   maxAge: 1000 * 60 * 60, // 세션 유효 기간 (예: 1시간)
+    // },
+  }),
+);
+
 const server = http.createServer(app);
+
+// app.use(function (req, res, next) {
+//   // res.header('Access-Control-Allow-Origin', req.headers.origin);
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept',
+//   );
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   next();
+// });
 
 // socket.io 서버 생성 및 옵션 설정
 export const io = new Server(server, {
@@ -39,37 +77,21 @@ export const io = new Server(server, {
   },
 });
 
-// 세션 설정
-const sessionConfig = {
-  secret: 'your-secret-key',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    secure: false, // HTTPS가 아닌 경우 false로 설정
-    maxAge: 1000 * 60 * 60, // 세션 유효 기간 (예: 1시간)
-  },
-};
-
-app.use(express.json());
-
 // cors
-app.use(
-  cors({
-    origin: true,
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-    maxAge: 86400,
-    optionsSuccessStatus: 200,
-    preflightContinue: true
-  }),
-);
+// app.use(
+//   cors({
+//     origin: true,
+//     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//     credentials: true,
+//     maxAge: 86400,
+//     optionsSuccessStatus: 200,
+//     preflightContinue: true,
+//   }),
+// );
 
-// Express 애플리케이션에 세션 미들웨어 추가
-app.use(session(sessionConfig));
-
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 // app.get('/', (req: Request, res: Response, next: NextFunction) => {
 //   res.sendFile(__dirname + '/index.html');
@@ -77,7 +99,7 @@ app.use(passport.session());
 
 app.use('/uploads', express.static('uploads'));
 
-app.listen(3000, () => {
+app.listen(8000, () => {
   console.log('server on!');
 });
 
@@ -87,9 +109,8 @@ server.listen(3002, () => {
 });
 
 //app.post('/auth/google', googleStrategy);
-app.post('/auth/google', googleLogin);
-app.get('/auth/google/callback', googleCallback);
-
+// app.post('/auth/google', googleLogin);
+// app.get('/auth/google/callback', googleCallback);
 
 app.use('/api/members', memberRouter);
 app.use('/api/admin', adminRouter);
@@ -98,88 +119,101 @@ app.use('/api/posts', postRouter);
 app.use('/api/reservations', reservationRouter);
 //app.use('/', authRouter);
 app.use('/api/chat', chatRouter);
+app.use('/api/access', accessRouter);
 
 
-// socket.io 연결
+/* socket.io 연결 */
 io.on('connect', socket => {
   console.log('connected!!!');
 
-  let currentRoomId: any;
+  // let currentRoomId: any;
 
-  // 채팅방 입장: 해당 채팅방의 모든 메세지 가져오기
+  /* 채팅방 입장: 해당 채팅방의 모든 메세지 가져오기 */
   socket.on('enterChatRoom', async (member_email: string) => {
-    // roomId 찾아오기
-    const roomId = await getRoomId(member_email);
+    try {
 
-    if (currentRoomId) {
-      socket.leave(currentRoomId);
-      console.log(`Left room ${currentRoomId}`);
-    }
+      if (!member_email) {
+        throw new Error('Required fields are missing.');
+      }
 
-    // 해당 방의 모든 메세지 가져오기
-    const allMessages = await getAllMessages(roomId);
-    console.log(allMessages);
-
-    // 가져온 메세지를 클라이언트로 전송
-    socket.emit('AllMessages', allMessages);
-  });
-
-  // 채팅방 생성
-  socket.on('createChatRoom', async (member_email: string, message: string) => {
-    // email에 해당하는 메세지 찾기
-    const checkChatRoomQuery = `SELECT room_id, sender_email, message, sentAt FROM chat_messages WHERE sender_email = ? LIMIT 1;`;
-    const [checkResult] = await con.promise().query(checkChatRoomQuery, [member_email]);
-
-    let roomId;
+      // roomId 찾아오기
+      const roomId = await getRoomId(member_email);
+      console.log(roomId);
   
-    if ((checkResult as RowDataPacket).length > 0) {
-      // 첫 메세지가 아닐 경우, 결과에서 roomId 가져오기
-      roomId = (checkResult as RowDataPacket)[0].room_id;
-      console.log("It's not the first msg. Got roomId!");
-    } else {
-      // 첫 메세지일 경우, 채팅방 생성
-      const createChatRoomQuery = `INSERT INTO chat_rooms (member_email) VALUES (?);`;
-      const [result] = await con.promise().query(createChatRoomQuery, [member_email]);
-      const newRoomId = (result as RowDataPacket).insertId;
-      roomId = newRoomId;
-      console.log('ChatRoom created!') 
-
-      // ROOM 입장
-      socket.join(roomId);
-      currentRoomId = roomId;
-      console.log(`Entered in ${roomId}!`);
+      // if (currentRoomId) {
+      //   socket.leave(currentRoomId);
+      //   console.log(`Left room ${currentRoomId}`);
+      // }
+  
+      if (roomId) {
+        console.log("It's not the first message. Got roomId!");
+      } else {
+        console.log('Room not found. Returning empty array.');
+        /* if (!방) {'메세지 없다'} -> null 전송 */
+        socket.emit('AllMessages', null);
+        return;
+      }
+  
+      // 해당 방의 모든 메세지 가져오기
+      const allMessages = await getAllMessages(roomId);
+      console.log(allMessages);
+  
+      /* 가져온 메세지를 클라이언트로 전송 */
+      socket.emit('AllMessages', allMessages);
+    } catch (error) {
+      console.error('채팅방 입장 중 에러 발생', error);
+      socket.emit('enterChatRoomError', '채팅방 입장 중 에러 발생');
     }
-
-    // 메세지 db 저장
-    await saveMessages(roomId, member_email, message);
-
-    // 해당 ROOM의 모든 메세지 전달
-    const allMessages = await getAllMessages(roomId);
-    socket.emit('AllMessages', allMessages);
-    console.log(allMessages);
   });
 
-  // 메세지 받고 주기 이벤트
+
+  /* 메세지 받고 주기: 첫 메세지일 경우 채팅방 생성 */
   socket.on('message', async (member_email: string, sender_email: string, message: string) => {
     // 소켓이 room 안에 있는지 확인
-    if (!currentRoomId) {
-      console.log("Socket is not in any room. Cannot send message.");
-      return;
+    // if (!currentRoomId) {
+    //   console.log("Socket is not in any room. Cannot send message.");
+    //   return;
+    // }
+    try {
+
+      if (!member_email || !sender_email || !message) {
+        throw new Error('Required fields are missing.');
+      }
+
+      let roomId;
+  
+      // 보낸 메세지 가져오기
+      const membersMessages = await getMembersMessages(member_email);
+    
+      if ((membersMessages as RowDataPacket).length > 0) {
+        roomId = (membersMessages as RowDataPacket)[0].room_id;
+        console.log("It's not the first msg. Got roomId!");
+      } else {
+        // 보낸 메세지가 없으면, 채팅방 생성 및 roomId 업데이트
+        const newRoomId = await createChatRoom(member_email);
+        roomId = newRoomId;
+        console.log('ChatRoom created!', roomId);
+  
+        /* socket ROOM 입장 */
+        socket.join(roomId);
+        // currentRoomId = roomId;
+        console.log(`Entered in ${roomId}!`);
+      }
+    
+      // 메세지 db 저장
+      await saveMessages(roomId, sender_email, message);
+      console.log('Messages saved!', message);
+  
+      /* 최신 메세지 전송 */
+      const latestMessage = await getLatestMessage(roomId);
+      socket.emit('message', latestMessage);
+  
+      /* 접속 유저 리스트 전송 */
+      const connectionData = await getAllConnectionData();
+      socket.emit('isOnline', connectionData);
+    } catch (error) {
+      console.error('메세지 처리 중 오류 발생', error);
+      socket.emit('messageError', '메세지 처리 중 오류 발생');
     }
-
-    // roomId 찾아오기
-    const roomId = await getRoomId(member_email);
-
-    // 메세지 db 저장
-    await saveMessages(roomId, sender_email, message);
-    console.log('Messages saved!', message);
-
-    // 최신 메세지 전송
-    const latestMessage = await getLatestMessage(roomId);
-    socket.emit('message', latestMessage);
-
-    // 접속 유저 리스트 전송
-    const connectionData = await getAllConnectionData();
-    socket.emit('isOnline', connectionData);
   })
 });
