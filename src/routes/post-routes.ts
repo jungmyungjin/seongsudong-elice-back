2c63f5a3b43a1aff4637f157de090c52b29dcd5a
Rki0 <pky00823@gmail.com>
2023-06-13 09:46:16 +0900

    feat: Post 관련 API 예외 처리 추가

--- src/routes/post-routes.ts
+++ src/routes/post-routes.ts
@@ -10,12 +10,16 @@ import {
   removePost,
 } from '../controllers/posts-controller';
 import upload from '../middlewares/upload-files';
+const checkAuth = require('../middlewares/check-auth');
 
 const postRouter = express.Router();
 
 postRouter.get('/', getPostList);
 postRouter.get('/recent', getRecentPosts);
 postRouter.get('/top', getTopPosts);
+
+postRouter.use(checkAuth);
+
 postRouter.post('/write', upload.array('file', 3), writePost);
 postRouter.get('/:postId', getPost);
 postRouter.patch('/:postId', upload.array('file', 3), editPost);
