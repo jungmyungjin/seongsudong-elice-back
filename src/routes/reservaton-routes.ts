571d66dc73ccb0a5165f780946a7fd5aec178fb5
[SW4/2반] 엄윤주 <yoonju.eom1@gmail.com>
2023-06-15 14:25:43 +0000

    Style: 불필요한 코드 삭제

--- src/routes/reservaton-routes.ts
+++ src/routes/reservaton-routes.ts
@@ -5,8 +5,6 @@ import {
 const checkAuth = require('../middlewares/check-auth');
 const router = express.Router();
 
-//router.use(checkAuth);
-
 // 예약 생성
 router.post('/', checkAuth, createReservation);
 
@@ -27,7 +25,6 @@ router.get('/users/:date', getReservationCountByDate);
 
 // 날짜별 예약건 수 조회
 router.get('/:date', getUserReservationCount)
-
 
 // 다른이메일로 예약정보 받기
 router.post('/send-email', sendEmailToUser);
